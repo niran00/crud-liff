192a01bb60f8dcd3e85d2b7d57024864170a6ed4
Niran <niran.millet@gmail.com>
2022-03-14 17:15:40 +0700

    express cores *

--- node-rest-api/index.js
+++ node-rest-api/index.js
@@ -27,7 +27,7 @@ app.use(bodyParser.urlencoded({
 app.use(cors());
 
 app.use(function (req, res, next){
-  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 })
