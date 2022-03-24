b8f07b47a4f2ab1e19605697a90b61ae6b47e009
Niran <niran.millet@gmail.com>
2022-03-24 14:21:53 +0700

    dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -14,13 +14,43 @@ export class DashboardComponent implements OnInit {
   constructor(
     private router: Router,
     private ngZone: NgZone,
-    private crudService: CrudService,
+    private crudService: CrudService
   ) { }
 
 
-
-  ngOnInit(): any {
+  @ViewChild('body') body: ElementRef | any  ;
+  @ViewChild('profile') profile: ElementRef | any  ;
+  @ViewChild('picutreUrl') picutreUrl: ElementRef | any  ;
+  @ViewChild('userId') userId: ElementRef | any  ;
+  @ViewChild('displayName') displayName: ElementRef | any  ;
+  @ViewChild('statusMessage') statusMessage: ElementRef | any  ;
+  @ViewChild('email') email: ElementRef | any  ;
   
+
+  async  main() {
+    liff.ready.then(() => {
+      if (liff.getOS() === 'android') {
+        this.body.nativeElement.style.backgroundColor = '#888';
+      }
+      if (liff.isInClient()) {
+        this.getUserProfile();
+      }
+    });
+    await liff.init({ liffId: '1656955187-j6JWxVQG' });
+  }
+  
+  async getUserProfile() {
+    const profile = await liff.getProfile();
+    this.picutreUrl.nativeElement.src = profile.pictureUrl;
+    this.userId.nativeElement.innerHTML = '<b>UserID:</b>' + profile.userId;
+    this.displayName.nativeElement.innerHTML = '<b>Display Name: </b>' + profile.displayName;
+    this.statusMessage.nativeElement.innerHTML = '<b>Status : </b>' + profile.statusMessage;
+    this.email.nativeElement.innerHTML = "<b>Email : </b>" + liff.getDecodedIDToken()?.email;
   }
 
+
+  ngOnInit(): void {
+    this.main();
+    this.getUserProfile();
+  }  
 }
