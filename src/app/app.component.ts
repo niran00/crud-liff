a25c1ed75cd854a2d44be43610bf4e46bb1187c1
Niran <niran.millet@gmail.com>
2022-03-24 14:13:42 +0700

    back to first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,45 +12,18 @@ type UnPromise<T> = T extends Promise<infer X>? X : T;
 })
 export class AppComponent implements OnInit {
 
-  messages: string;
-  userProfile: any;
-  
+
   constructor(
     private lineService: LineService,
-  ) { 
-    this.messages = "";
-    this.initLineLiff();
-  }
+  ) { }
 
-  async ngOnInit() {
-    await this.initLineLiff();
-  }
+  LineValues:any = [];  
 
-  async initLineLiff() {
-    try {
-      const data: any = await this.lineService.initLineLiff();
-      this.userProfile = await liff.getProfile();
-      alert(`Hi ${this.userProfile.displayName}!`);
-    } catch (err) {
-      // alert(err)
-    }
-  }
+  os: ReturnType<typeof liff.getOS>;  
+  profile!: UnPromise<ReturnType<typeof liff.getProfile>>;
 
-  async sendMessages() {
-    try {
-      const successMsgs = await liff.sendMessages([
-        {
-          type: 'text',
-          text: this.messages
-        }
-      ])
-      liff.closeWindow()
-
-
-    } catch (e) {
-      alert(e)
-    }
-
+  ngOnInit(): any {
+    this.lineService.getLineData();
   }
 
   
