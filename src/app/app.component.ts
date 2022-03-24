944d291b182359deeb308d4100c119c6fc68999b
Niran <niran.millet@gmail.com>
2022-03-24 14:05:42 +0700

    pulled code x

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,17 +12,45 @@ type UnPromise<T> = T extends Promise<infer X>? X : T;
 })
 export class AppComponent implements OnInit {
 
-
+  messages: string;
+  userProfile: any;
+  
   constructor(
     private lineService: LineService,
-  ) { }
+  ) { 
+    this.messages = "";
+    this.initLineLiff();
+  }
+
+  async ngOnInit() {
+    await this.initLineLiff();
+  }
+
+  async initLineLiff() {
+    try {
+      const data: any = await this.lineService.initLineLiff();
+      this.userProfile = await liff.getProfile();
+      alert(`Hi ${this.userProfile.displayName}!`);
+    } catch (err) {
+      // alert(err)
+    }
+  }
+
+  async sendMessages() {
+    try {
+      const successMsgs = await liff.sendMessages([
+        {
+          type: 'text',
+          text: this.messages
+        }
+      ])
+      liff.closeWindow()
 
 
-  os: ReturnType<typeof liff.getOS>;  
-  profile!: UnPromise<ReturnType<typeof liff.getProfile>>;
+    } catch (e) {
+      alert(e)
+    }
 
-  ngOnInit(): any {
-    this.lineService.getLineData();
   }
 
   
--- src/app/service/line.service.ts
+++ src/app/service/line.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
-import liff from '@line/liff';
+
 import * as liffApi from '@liff/is-api-available';
 import { Observable, Subject } from 'rxjs';
 
 type UnPromise<T> = T extends Promise<infer X>? X : T;
-
+declare var liff: any;
 @Injectable({
   providedIn: 'root'
 })
@@ -12,26 +12,46 @@ export class LineService {
 
   
 
-  os: ReturnType<typeof liff.getOS>;  
-  profile!: UnPromise<ReturnType<typeof liff.getProfile>>;
-  private liffValues: any = [];
+  // os: ReturnType<typeof liff.getOS>;  
+  // profile!: UnPromise<ReturnType<typeof liff.getProfile>>;
+  // private liffValues: any = [];
   
-  getLineData() { 
-    liff.init({liffId:'1656955187-j6JWxVQG'}).then(()=>{
-      this.os=liff.getOS();
-      if(liff.isLoggedIn()){
-        liff.getProfile().then( profile =>{
-          this.profile = profile;
-          this.liffValues.push(this.profile.displayName);
-          return this.liffValues;
-        }).catch(console.error);
-      }else{
-        liff.login()
-      }
-    }).catch(console.error);
+  // getLineData() { 
+  //   liff.init({liffId:'1656955187-j6JWxVQG'}).then(()=>{
+  //     this.os=liff.getOS();
+  //     if(liff.isLoggedIn()){
+  //       liff.getProfile().then( profile =>{
+  //         this.profile = profile;
+  //         this.liffValues.push(this.profile.displayName);
+  //         return this.liffValues;
+  //       }).catch(console.error);
+  //     }else{
+  //       liff.login()
+  //     }
+  //   }).catch(console.error);
     
-  }
+  // }
   
+  
+  initLineLiff() {
+    return new Promise((resolve, reject) => {
+      liff.init(data => {
+        resolve(liff.getProfile())
+      }, err=>{
+        reject(err)
+      })
+    })
+  }
+
+  getLineProfile() {
+    return new Promise((resolve, reject) => {
+      liff.getProfile(data => {
+        resolve(data)
+      }, err=>{
+        reject(err)
+      })
+    })
+  }
 
   constructor() { }
 }
