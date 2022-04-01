6cdd1ffb541437ab83ec94e7050dd3aebc35f6dc
Niran <niran.millet@gmail.com>
2022-04-01 15:24:05 +0700

    commiting auth gard off

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -21,8 +21,7 @@ const routes: Routes = [
   },  
   { 
     path: 'dashboard',
-    component: DashboardComponent, 
-    canActivate: [AuthGuard] 
+    component: DashboardComponent
   },
   { 
     path: 'login',
