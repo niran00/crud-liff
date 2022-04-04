bc3de4339fd533e86a910855654deab5c44f7e28
Niran <niran.millet@gmail.com>
2022-04-04 10:31:57 +0700

    make the Id

--- src/app/components/add-user/add-user.component.ts
+++ src/app/components/add-user/add-user.component.ts
@@ -39,7 +39,8 @@ export class AddUserComponent implements OnInit {
 
   async getUserProfile() {
     const profile = await liff.getProfile();
-    this.theUserId = profile.userId;
+    const maketheId = profile.userId;
+    this.theUserId = maketheId;
   }
 
   
