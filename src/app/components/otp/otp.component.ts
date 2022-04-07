68c19209a8cf43bc29cf81858cb5a84a22b2d8f0
Niran <niran.millet@gmail.com>
2022-04-07 14:55:52 +0700

    otp login

--- src/app/components/otp/otp.component.ts
+++ src/app/components/otp/otp.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, NgZone, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserService } from './../../service/user.service';
+import liff from '@line/liff';
+import * as liffApi from '@liff/is-api-available';
+
+
 @Component({
   selector: 'app-otp',
   templateUrl: './otp.component.html',
@@ -11,6 +16,7 @@ export class OtpComponent implements OnInit {
   constructor(
     private router: Router,
     private ngZone: NgZone,
+    private userService : UserService,
   ) { 
     
   }
@@ -19,16 +25,37 @@ export class OtpComponent implements OnInit {
 
   @ViewChild('ngOtpInput') ngOtpInputRef:any;
 
+  async  main() {
+    liff.ready.then(() => {
+      if (liff.getOS() === 'android') {
+       
+      }
+      if (liff.isInClient()) {
+        this.getUserProfile();
+      }
+    });
+    await liff.init({ liffId: '1656955187-j6JWxVQG' });
+  }
+  
+  theId : string = '';
+  async getUserProfile() {
+    const profile = await liff.getProfile();
+    const theId = profile.userId;
+  }
+
   onOtpChange(otp: string | any[] ) {
     this.otp = otp;
     if (otp.length > 3) {
+
+      this.userService.login(this.theId);
       this.ngZone.run(() => this.router.navigateByUrl('/dashboard'))
     }
   }
 
   ngOnInit(): void {
 
-    
+    this.main();
+    this.getUserProfile(); 
   }
 
 }
